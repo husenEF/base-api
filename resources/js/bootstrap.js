968c5fd14b550472f2d6ac9cc9f9799ab0a481a5
Husen Efendi <husenef@gmail.com>
2019-11-11 16:33:03 +0700

    add minimal running vue+botstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,13 +10,9 @@ try {
     window.$ = window.jQuery = require("jquery");
 
     require("bootstrap");
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+} catch (e) {
+    console.log("error", e);
+}
 
 window.axios = require("axios");
 
