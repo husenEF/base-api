97dca5fe0d56e8be66cc569895b51fcfd83121f9
Husen Efendi <husenef@gmail.com>
2019-11-20 16:23:12 +0700

    add example data with auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,7 +36,9 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            $response = ['data' => '', "message" => 'Unauthorized', "code" => 401];
+            // return response('Unauthorized.', 401);
+            return response($response, 401);
         }
 
         return $next($request);
--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,8 @@ $router->group(['namespace' => 'v1', 'prefix' => '/v1'], function ($router) {
      */
     $router->group(['prefix' => '/auth'], function ($router) {
         $router->post('login', 'AuthController@login');
+        $router->group(['middleware' => 'auth:api'], function ($router) {
+            $router->get('/myprofile', 'UserController@index');
+        });
     });
 });
