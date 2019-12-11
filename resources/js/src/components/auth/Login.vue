f5e35da0a732f98437df264dae3afa4066ac96c7
Husen Efendi <husenef@gmail.com>
2019-12-11 17:42:02 +0700

    update sotre js'

--- resources/js/src/components/auth/Login.vue
+++ resources/js/src/components/auth/Login.vue
@@ -5,15 +5,19 @@
         <div class="card-heder">
           <h2 class="text-center">Login</h2>
         </div>
+        <div class="alert alert-danger" role="alert" v-if="errorMessage">{{errorMessage}}</div>
         <div class="card-body">
-          <form action>
+          <form @submit.prevent="submitData">
             <div class="form-group row">
               <label for>Username</label>
-              <input type="text" name="username" class="form-control" />
+              <input type="text" name="username" class="form-control" v-model="form.username" />
             </div>
             <div class="form-group row">
               <label for>Password</label>
-              <input type="password" name="password" id class="form-control" />
+              <input type="password" name="password" id class="form-control" v-model="form.pass" />
+            </div>
+            <div class="form-group row">
+              <button type="submit" class="btn btn-primary">Login</button>
             </div>
           </form>
         </div>
@@ -24,7 +28,27 @@
 
 <script>
 export default {
-  name: "LoginPage"
+  name: "LoginPage",
+  data() {
+    return {
+      form: {
+        username: "",
+        pass: ""
+      }
+    };
+  },
+  computed: {
+    errorMessage() {
+      return this.$store.getters.getError("login");
+    }
+  },
+  methods: {
+    submitData() {
+      this.$store.dispatch("loginAction", this.form);
+      // .then(() => this.$router.push("/"))
+      // .catch(err => console.log("err login", err));
+    }
+  }
 };
 </script>
 
--- resources/js/store.js
+++ resources/js/store.js
@@ -1,14 +1,55 @@
-
 export default {
     state: {
         welcomeMessage: "Welcome to my vue App",
-        isLoggedIn: !!localStorage.getItem('token')
+        isLoggedIn: !!localStorage.getItem('token'),
+        user: {},
+        status: '',
+        token: '',
+        errorMessage: {
+            login: ''
+        }
     },
     getters: {
         welcome(state) {
             return state.welcomeMessage;
+        },
+        getError: state => index => {
+            return state.errorMessage[index]
         }
     },
-    mutations: {},
-    actions: {}
+    mutations: {
+        auth_success(state, token, user) {
+            state.isLoggedIn = true
+            state.user = user
+            state.status = 'success'
+            state.token = token
+
+        },
+        auth_error(state, message) {
+            state.status = 'error'
+            state.user = {}
+            state.errorMessage = { ...state.errorMessage, login: message }
+        },
+    },
+    actions: {
+        loginAction({ commit }, user) {
+            return new Promise((resolve, reject) => {
+                axios.post('/api/v1/auth/login', { email: user.username, password: user.pass })
+                    .then(response => {
+                        const { data: { data: { token, expires, user } } } = response
+                        commit('auth_success', token, user)
+                        localStorage.setItem('token', token)
+                        localStorage.setItem('user', user)
+                        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+                        resolve(response)
+                    })
+                    .catch(err => {
+                        const { response: { data: { message } } } = err
+                        commit('auth_error', message)
+                        localStorage.removeItem('token')
+                        reject(err)
+                    })
+            })
+        }
+    }
 };
